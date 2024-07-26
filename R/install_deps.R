233c25c7a6368ea3d90562b9d0a7be128ef4e094
Marcel Ferreira <marcelrodrigues45@yahoo.com.br>
2024-07-26 20:21:19 -0300

    RcppFadeeva added

---
+++ R/install_deps.R
@@ -0,0 +1,10 @@
+# R/install_deps.R
+
+install_github_dependency <- function() {
+  if (!requireNamespace("RcppFaddeeva", quietly = TRUE)) {
+    message("Installing RcppFaddeeva from GitHub...")
+    remotes::install_github("marceelrf/RcppFaddeeva")
+  }
+}
+
+install_github_dependency()
---
+++ R/zzz.R
@@ -0,0 +1,10 @@
+# R/zzz.R
+
+.onLoad <- function(libname, pkgname) {
+  install_deps_path <- system.file("R", "install_deps.R", package = pkgname)
+  if (file.exists(install_deps_path)) {
+    source(install_deps_path)
+  } else {
+    warning("install_deps.R not found in the package.")
+  }
+}
